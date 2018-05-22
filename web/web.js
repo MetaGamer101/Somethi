3bed5dbd5181444c393e7185325e6420d20ec24c
Squashbucklr <jbwar22@gmail.com>
2018-05-21 20:45:58 -0400

    website okish

--- web/web.js
+++ web/web.js
@@ -3,8 +3,9 @@ var router = express.Router();
 
 module.exports = router;
 
-var routeResources = require('./routes/resource.js');
-router.use('/resources', routeResources);
+//var routeResources = require('./routes/resource.js');
+//router.use('/resources', routeResources);
+router.use('/resources', express.static(__dirname + '/resources/web'));
 
 var routeHome = require('./routes/page/home.js');
 router.get('/', routeHome);
